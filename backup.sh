bb857dc909928fe694baa222485e805812fdd181
Vladislav Gritsenko <vladgritsenko@gmail.com>
2017-01-08 21:48:37 +0200

    fixed backuping

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ BACKUPS="/backups/";
 BASEDIR=$(dirname "$0")
 
 mkdir -p $BASEDIR$BACKUPS;
-/usr/bin/mysqldump -u root -p"y9Dt6^f2fNU;8RLU4{xP" ncrp | gzip -v > $FILENAME;
+/usr/bin/mysqldump -u root -p"y9Dt6^f2fNU;8RLU4{xP" ncrp | gzip -v > $BASEDIR$BACKUPS$FILENAME;
 
 echo "created backup $FILENAME";
